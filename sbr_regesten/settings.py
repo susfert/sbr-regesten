5f9b58a2241cefdcc6bee7f18be048768aefd466
Tim Krones <t.krones@gmx.net>
2012-10-08 11:16:29 +0200

    Import Django templates relevant for customizing the admin interface

--- sbr_regesten/settings.py
+++ sbr_regesten/settings.py
@@ -1,5 +1,9 @@
 # Django settings for sbr_regesten project.
 
+from os import getcwd
+
+ROOT_PATH = getcwd()
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -106,6 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '{0}/templates'.format(ROOT_PATH)
 )
 
 INSTALLED_APPS = (
