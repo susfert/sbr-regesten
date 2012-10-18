df9a8c393a7f96dbca848ff47c68a4db1bb8f252
Tim Krones <t.krones@gmx.net>
2012-10-18 18:35:40 +0200

    Enable the Quote model in the admin interface

--- regesten_webapp/admin.py
+++ regesten_webapp/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from django.contrib.contenttypes.generic import GenericStackedInline
 from django.contrib.sites.models import Site
 
 from regesten_webapp.models import Archive, Concept, Family
 from regesten_webapp.models import Footnote, Landmark, Location
-from regesten_webapp.models import Person, PersonGroup, Regest
-from regesten_webapp.models import RegestDate, Region
+from regesten_webapp.models import Person, PersonGroup, Quote
+from regesten_webapp.models import Regest, RegestDate, Region
 
 
 class RegestDateInline(admin.StackedInline):
@@ -16,21 +17,30 @@ class ArchiveInline(admin.StackedInline):
 class FootnoteInline(admin.StackedInline):
     model = Footnote
 
+class QuoteInline(GenericStackedInline):
+    model = Quote
+
 class RegestAdmin(admin.ModelAdmin):
     inlines = [
         RegestDateInline,
         ArchiveInline,
         FootnoteInline,
+        QuoteInline
+        ]
+
+class ConceptAdmin(admin.ModelAdmin):
+    inlines = [
+        QuoteInline,
         ]
 
 
 admin.site.unregister(Site)
 
 admin.site.register(Regest, RegestAdmin)
-admin.site.register(Concept)
-admin.site.register(Landmark)
-admin.site.register(Location)
-admin.site.register(Person)
-admin.site.register(PersonGroup)
-admin.site.register(Family)
+admin.site.register(Concept, ConceptAdmin)
+admin.site.register(Landmark, ConceptAdmin)
+admin.site.register(Location, ConceptAdmin)
+admin.site.register(Person, ConceptAdmin)
+admin.site.register(PersonGroup, ConceptAdmin)
+admin.site.register(Family, ConceptAdmin)
 admin.site.register(Region)
