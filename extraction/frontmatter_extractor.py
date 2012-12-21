d811ff74c58d9b7d0cb1f4fe8561c0a6b450bd3c
Tim Krones <t.krones@gmx.net>
2012-12-22 00:34:36 +0100

    Implement first working version of frontmatter extraction

---
+++ extraction/frontmatter_extractor.py
@@ -0,0 +1,36 @@
+""" This module extracts the front matter part of the Sbr Regesten. """
+
+import codecs
+from bs4 import BeautifulSoup
+
+text = codecs.open('../html/sbr-regesten.html', 'r', 'cp1252').read()
+
+soup = BeautifulSoup(text)
+
+frontmatter = []
+
+inside_if = False
+
+for line in soup.get_text().split('\n'):
+    if not inside_if:
+        if '<!--' in line or '[if' in line:
+            inside_if = True
+            continue
+        if line.startswith('Inhaltsverzeichnis'): break
+        if line and not line == u'\xa0': frontmatter.append(line)
+    else:
+        if '[endif]' in line and not '[if' in line:
+            inside_if = False
+
+
+def write_with_indent(file, string, indent_level):
+    spaces = indent_level * ' '
+    file.write(spaces+string)
+
+
+with codecs.open('../sbr-regesten.xml', 'a', 'utf-8') as xmlfile:
+    write_with_indent(xmlfile, '<sbr-regesten>\n', 0)
+    write_with_indent(xmlfile, '<frontmatter>\n', 2)
+    for line in frontmatter:
+        write_with_indent(xmlfile, line+'\n', 4)
+    write_with_indent(xmlfile, '</frontmatter>\n', 2)
