00b5d8be16b477da097994fa1ae753236a53b5f2
Susanne Fertmann <s9sufert@stud.uni-saarland.de>
2012-12-31 15:08:45 +0100

    Add docString

--- extraction/index_extractor.py
+++ extraction/index_extractor.py
@@ -1,3 +1,10 @@
+"""
+This module extracts the index from the Sbr Regesten.
+
+Author: Susanne Fertmann <s9sufert@stud.uni-saarland.de>
+"""
+
+
 from regesten_webapp import models
 from index_to_xml import *
 from index_to_db import *
@@ -5,7 +12,9 @@ from index_xml_postprocess import *
 
 
 def extract_index():
+ #index_to_xml()      # extracts forenames
  index_to_xml()
- #index_to_xml()
  index_xml_postprocess()
  index_to_db()
+
+
