5bf8ffcc7aa029a499ccc20678f6af556b141c16
Susanne Fertmann <s9sufert@stud.uni-saarland.de>
2013-01-28 23:05:46 +0100

    Tmp-file is now deleted after using + some style adaptions.

--- extraction/bibliography_extractor.py
+++ extraction/bibliography_extractor.py
@@ -1,69 +1,68 @@
 """ This module extracts the bibliography of the Sbr Regesten. """
 
-import codecs, re
+import codecs
+import re
+import glob
+import os
 from bs4 import BeautifulSoup
 
-#print('reached bibl file')
-
 def indent_bibl():
-  with codecs.open('bibl.xml', 'r', 'utf-8') as biblfile:
-    with codecs.open('sbr-regesten.xml', 'a', 'utf-8') as xmlfile:
-      for line in biblfile:
-        xmlfile.write('  '+line)
+    '''Indents the bibliography before writing it into sbr-regesten.xml'''
+    with codecs.open('bibl_tmp.xml', 'r', 'utf-8') as biblfile:
+        with codecs.open('sbr-regesten.xml', 'a', 'utf-8') as xmlfile:
+            for line in biblfile:
+                xmlfile.write('  ' + line)
 
 
 def extract_bibliography():
-  print ('extracting bibliography..')
-  #text = codecs.open('html/sbr-regesten.html', 'r', 'cp1252').read()
-  text = codecs.open('html/sbr-regesten.html', 'r', 'cp1252').read()
+    '''Extracts the bibliography part of the sbr-regensten from sbr-regesten.html.'''
+    print ('extracting bibliography..')
+    text = codecs.open('html/sbr-regesten.html', 'r', 'cp1252').read()
 
-  text=unicode(text)
-  soup = BeautifulSoup(text)
+    text = unicode(text)
+    soup = BeautifulSoup(text)
 
-  id=0
-  #bibls=[]
-  foundBibl = False
-  #print ('extracting bibliography..')
-  biblListTag=soup.new_tag('listBibl')
-  biblInfoTag= soup.new_tag('listBibl-info')
-  biblListTag.append(biblInfoTag)
+    id = 0
+    foundBibl = False
+    biblListTag = soup.new_tag('listBibl')
+    biblInfoTag = soup.new_tag('listBibl-info')
+    biblListTag.append(biblInfoTag)
 
-  htmlItems=soup.findAll('p')
-  foundBibl=False
-  nextBiblInfo=False
-  for htmlItem in htmlItems:
-    if htmlItem.get_text().strip()!='' and htmlItem.get_text()!='\n' and htmlItem.get_text()!= "\r":
-      if htmlItem.get_text().startswith('Abk'):
-        break
-      
-      if htmlItem.get_text().startswith('Literaturverzeichnis'):
-        biblListTag.append(htmlItem.get_text())
-        foundBibl=True
-        nextBiblInfo=True
-        print('Bibl gefunden')
-        continue
-       
-      elif nextBiblInfo:
-        print('found bibl-info')
-        biblInfoTag.append(htmlItem.get_text())
-        biblListTag.append(biblInfoTag)
-        #biblInfoTag.append('\n')
-        nextBiblInfo=False
-        continue
+    htmlItems = soup.findAll('p')
+    foundBibl = False
+    nextBiblInfo = False
+    for htmlItem in htmlItems:
+        item_text = htmlItem.get_text()
+        if htmlItem.get_text().strip() != '' and item_text != '\n' and item_text != '\r':
+            if item_text.startswith('Abk'):
+                break
+            
+            if item_text.startswith('Literaturverzeichnis'):
+                biblListTag.append(item_text)
+                foundBibl = True
+                nextBiblInfo = True
+                continue
+             
+            elif nextBiblInfo:
+                biblInfoTag.append(item_text)
+                biblListTag.append(biblInfoTag)
+                nextBiblInfo = False
+                continue
 
-      if foundBibl:
-        item=htmlItem.get_text()
-        biblTag=soup.new_tag('bibl')
-        biblTag['id']='bibl_'+str(id)
-        id+=1
-        biblTag.append(item)
-        biblListTag.append(biblTag)
+            if foundBibl:
+                item = htmlItem.get_text()
+                biblTag=soup.new_tag('bibl')
+                biblTag['id'] = 'bibl_' + str(id)
+                id += 1
+                biblTag.append(item)
+                biblListTag.append(biblTag)
 
-  with codecs.open('bibl.xml', 'w', 'utf-8') as biblfile:
-    biblfile.write('\n'+unicode(biblListTag.prettify())+'\n')
-  
-  indent_bibl()
+    with codecs.open('bibl_tmp.xml', 'w', 'utf-8') as biblfile:
+        biblfile.write('\n' + unicode(biblListTag.prettify()) + '\n')
+    
+    indent_bibl()
+    os.remove('bibl_tmp.xml')
 
 
-if __name__=='main':
-  extract_bibliography()
+if __name__ == 'main':
+    extract_bibliography()
